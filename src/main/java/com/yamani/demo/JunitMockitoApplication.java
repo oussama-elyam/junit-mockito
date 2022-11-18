d92544f6fa355140afeb386dce27a2272334d757
elyamart <elyamanioussama3@gmail.com>
2022-11-18 11:20:46 +0100

    first commit

---
+++ src/main/java/com/yamani/demo/JunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.yamani.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yamani/demo/JunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamani.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
